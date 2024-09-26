c624572e79a4f51cc68a26901d2a12853f1d4632
manuelthegoat <manuelcreation247@gmail.com>
2024-09-26 10:06:43 +0100

    Made possible changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './Footer.css'; // Import the CSS file if needed
+import './Footer.css'; // Make sure you import the CSS file
 
 const Footer = () => {
   return (
     <footer className="afooter">
-      <Container>
-        <Row>
-          <Col className="left-center">
-            <p>&copy; {new Date().getFullYear()} Niba. All rights reserved.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="resources-section">
-              <h3>Resources</h3>
-              <ul>
-                <li><a href="/api-resources">API Resources</a></li>
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/faq">Frequently Asked Questions</a></li>
-              </ul>
-            </div>
+      <Container fluid>
+        {/* First Row: Branding and Links */}
+        <Row className="justify-content-center py-4">
+  {/* Branding Section */}
+  <Col md={3} className="text-center">
+    <h5>Niba</h5>
+    <p>&copy; {new Date().getFullYear()} Niba. All rights reserved.</p>
+  </Col>
+
+  {/* Resources Section */}
+  <Col md={3} className="text-center resources-section">
+    <h5>Resources</h5>
+    <ul>
+      <li><a href="/api-resources">API Resources</a></li>
+      <li><a href="/blog">Blog</a></li>
+      <li><a href="/faq">FAQ</a></li>
+    </ul>
+  </Col>
+
+  {/* Contact Section */}
+  <Col md={3} className="text-center contact-section">
+    <h5>Contact Us</h5>
+    <ul>
+      <li><a href="/contact">Contact Form</a></li>
+      <li><a href="mailto:support@niba.com">support@niba.com</a></li>
+      <li><a href="tel:+123456789">+1 (234) 567-89</a></li>
+    </ul>
+  </Col>
+</Row>
+
+
+        {/* Second Row: Social Media Links */}
+        <Row className="justify-content-center py-3">
+          <Col md={6} className="social-media">
+            <h5>Follow Us</h5>
+            <ul>
+              <li><a href="https://facebook.com">Facebook</a></li>
+              <li><a href="https://twitter.com">Twitter</a></li>
+              <li><a href="https://instagram.com">Instagram</a></li>
+              <li><a href="https://linkedin.com">LinkedIn</a></li>
+            </ul>
           </Col>
         </Row>
       </Container>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Nidia</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/services">Services</Nav.Link>
-          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand as={Link} to="/">Nidia</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+          <Button variant="outline-light" as={Link} to="/get-started">
+            Login
+          </Button>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
